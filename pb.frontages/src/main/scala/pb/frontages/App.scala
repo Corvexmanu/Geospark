dcf7cfc2c1455218cb39e368451bafa51e711829
Victor Manuel Villamil Romero <n10080236@qut.edu.au>
2019-02-06 11:48:45 +1000

    Modification Feb

--- pb.frontages/src/main/scala/pb/frontages/App.scala
+++ pb.frontages/src/main/scala/pb/frontages/App.scala
@@ -1,5 +1,6 @@
 package pb.frontages
 
+import java.io._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.datasyslab.geospark.formatMapper.shapefileParser.ShapefileReader
@@ -15,12 +16,19 @@ import org.datasyslab.geospark.spatialOperator.KNNQuery;
 import org.datasyslab.geospark.spatialRDD.RectangleRDD;
 import org.datasyslab.geospark.spatialOperator.JoinQuery;
 import org.datasyslab.geospark.enums.GridType;
+import scala.collection.JavaConversions._
+import au.com.bytecode.opencsv.CSVWriter
+import java.io.{BufferedWriter, FileWriter}
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ListBuffer
+import java.io.File
+import scala.io.Source
+import java.io.PrintWriter
 
 object App 
 {
   
-  def main(args: Array[String]) 
-  {
+  def main(args: Array[String]){
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]");
     
     val sc = new SparkContext(conf)
@@ -29,35 +37,55 @@ object App
     val splitter = FileDataSplitter.CSV
     val storage = StorageLevel.MEMORY_ONLY
     val indexType = IndexType.RTREE
-    val grid = GridType.EQUALGRID
+    val grid = GridType.EQUALGRID    
     
-    
-    SpatialRangeQueryWithoutIndex(sc,path_CAD)
     KnnSpatialQuery (sc,path_CAD)
+    SpatialRangeQueryWithoutIndex(sc,path_CAD)
   }
   
-  def SpatialRangeQueryWithoutIndex (sc:SparkContext, path:String)
-  {
-    val queryEnvelope=new Envelope (-35.45149572, -35.44764606, 138.3153426, 138.3195838);
+  
+  def SpatialRangeQueryWithoutIndex (sc:SparkContext, path:String){
+    val queryEnvelope=new Envelope (138.316576, 138.317247, -35.454158, -35.453624);
     val CadastralRDD = ShapefileReader.readToPolygonRDD(sc, path)  
-    val resultSize = RangeQuery.SpatialRangeQuery(CadastralRDD, queryEnvelope, false, false); /* The O means consider a point only if it is fully covered by the query window when doing query */
-    println(resultSize.count())
-    println(resultSize)
-    CadastralRDD.saveAsGeoJSON("D:\\WIP\\Project_Frontages_and_Setbacks\\InputDatasets\\Small_Dataset\\GeoJSON_output\\temp")
-    println("File Saved")
-  }
+    val result2 = RangeQuery.SpatialRangeQuery(CadastralRDD, queryEnvelope, false, false); /* The O means consider a point only if it is fully covered by the query window when doing query */
+    var polyg = 0
+    val writer2 = new PrintWriter(new File("D:\\WIP\\Project_Frontages_and_Setbacks\\Output\\csv\\Write2.txt"))
+    writer2.write("POLYGON"+","+"NODE"+","+"X"+","+"Y"+"\r\n")
+    Source.fromFile("D:\\WIP\\Project_Frontages_and_Setbacks\\Output\\csv\\Write2.txt").foreach { x => print(x)  }
+    result2.collect() foreach {poly =>
+      polyg = polyg + 1
+      var node = 0
+      poly.getCoordinates() foreach {coor =>
+        node = node + 1
+        writer2.write(polyg + "," + node + ","+ coor.getOrdinate(0) + "," + coor.getOrdinate(1) +"\r\n")
+        Source.fromFile("D:\\WIP\\Project_Frontages_and_Setbacks\\Output\\csv\\Write2.txt").foreach { x => print(x) }
+                                    }     
+                               }            
+    writer2.close()
+    println("Elkin is ugly")
+    }
   
-  def KnnSpatialQuery (sc:SparkContext, path:String)
-  {
+  def KnnSpatialQuery (sc:SparkContext, path:String) {
     val geometryFactory = new GeometryFactory()
     val CadastralRDD = ShapefileReader.readToPolygonRDD(sc, path) 
-    val pointObject = geometryFactory.createPoint(new Coordinate(-35.452771, 138.312686))
+    val pointObject = geometryFactory.createPoint(new Coordinate(138.312686,-35.452771))
     val K = 1 // K Nearest Neighbors
     val usingIndex = false
-    val resultSize = KNNQuery.SpatialKnnQuery(CadastralRDD, pointObject, K, usingIndex)
-    println(resultSize)
-    println(pointObject)
-    println("KNN executed")
-  }
-  
+    val result = KNNQuery.SpatialKnnQuery(CadastralRDD, pointObject, K, usingIndex)
+    var polyg = 0
+    val writer = new PrintWriter(new File("D:\\WIP\\Project_Frontages_and_Setbacks\\Output\\csv\\Write.txt"))
+    writer.write("POLYGON"+","+"NODE"+","+"X"+","+"Y"+"\r\n")
+    Source.fromFile("D:\\WIP\\Project_Frontages_and_Setbacks\\Output\\csv\\Write.txt").foreach { x => print(x)  }
+    result foreach {poly =>
+      polyg = polyg + 1
+      var node = 0
+      poly.getCoordinates() foreach {coor =>
+        node = node + 1
+        writer.write(polyg + "," + node + ","+ coor.getOrdinate(0) + "," + coor.getOrdinate(1) +"\r\n")
+        Source.fromFile("D:\\WIP\\Project_Frontages_and_Setbacks\\Output\\csv\\Write.txt").foreach { x => print(x) }
+                                    }     
+                    }
+    writer.close()
+    println("Knn Executed")
+    } 
 }
